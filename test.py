55caf5411ad185dd8edcef612a07c5bc9676bccd
banagaurav <gauravbana840@gmail.com>
2026-01-02 12:22:31 +0545

    reading files and example.

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import pandas as pd
+# This is how we read/load csv(Comma Seperated File)
 df = pd.read_csv(r"D:\Code\PipPandas\world_population.csv")
+# Too read file in other formats is easy
+# read_json for json files 
+# read_excel for excel @ easy right ! 
 print(df)
